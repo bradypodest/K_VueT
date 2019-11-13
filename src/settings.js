79a8ae565b3b6472018b100bf2d5e86fa67bc1d3
KarlOld <1203713708@qq.com>
2019-11-14 00:14:55 +0800

    first commit

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+
+//全局设置
+module.exports = {
+
+  title: 'K.VUE',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
