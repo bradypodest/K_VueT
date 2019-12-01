45f2ab470b4a6d6aa492a1dab02a226caa332642
KarlOld <1203713708@qq.com>
2019-12-01 13:53:56 +0800

    加入图标库   复制插件clipboard

--- src/views/system/sysMenu.vue
+++ src/views/system/sysMenu.vue
@@ -17,7 +17,7 @@
     <el-table
       :data="tableData"
       row-key="ID"
-      default-expand-all
+      
       :tree-props="{children: 'Children', hasChildren: 'hasChildren'}"
       border
       style="width: 100%;box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"
